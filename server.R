f8cb6a568b4de0f96ad46f109455a5ebc923d7af
osenan <oriol.senan@urv.cat>
2018-12-11 18:35:00 +0100

    reactive function added

--- server.R
+++ server.R
@@ -3,13 +3,28 @@ library(cliqueMS)
 
 
 server <- function(input, output) {
-    output$table <- renderTable({
+    getpeaklist <- reactive({
         if ( is.null(input$file)) return(NULL)
         inFile <- isolate({input$file })
-        file <- inFile$datapath
-        load(file = file, envir = .GlobalEnv)
-        anC <- cliqueMS::getCliques(standard)
-        return(anC$peaklist)
+        filename <- inFile$datapath 
+        msSet <- readRDS(filename)
+        anC <- cliqueMS::getCliques(msSet)
+        peaklist <- anC$peaklist
+        return(peaklist)
+    })
+    
+    output$histCliques <- renderPlot({
+        peaklist <-  req(getpeaklist())
+        hist(table(peaklist$cliqueGroup), col = "blue", border = "white",
+             xlab = "Clique size (number of features)", ylab = "Counts",
+             main = "Clique group size histogram")
+    }
+    )
+    output$table <- renderTable({
+        peaklist <- req(getpeaklist())
+        df <- data.frame("Features" = nrow(peaklist),
+                         "cliqueGroups" = length(unique(peaklist$cliqueGroup))
+                         )
     }
     )
 }
